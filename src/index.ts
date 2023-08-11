831acbe8d52fe1e4e1cef7f37498f2cb971905ce
Richard Guach <guachrichard@gmail.com>
2023-08-11 01:48:37 -0500

    cors implemented

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, {Express} from 'express'
 import productRoutes from './routes/products.routes.ts'
 import providerRoutes from './routes/providers.routes.ts'
+import cors from 'cors';
 
 
 const port = 3000;
@@ -8,6 +9,7 @@ const app:Express = express();
 
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api',productRoutes);
 app.use('/api',providerRoutes);
